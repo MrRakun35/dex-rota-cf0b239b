af81e652dbfce11d2812ac4c523d4e601a37ed1b
orderly-guoqing <guoqing@orderly.network>
2026-09-23 21:45:12 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -22,7 +22,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Swap,Rewards",
   "VITE_CUSTOM_MENUS": "AlgoBotApp,https://algobotapp.com/",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
-  "VITE_ENABLE_CAMPAIGNS": "true",
+  "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/rota_finance",
   "VITE_DISCORD_URL": "",
   "VITE_TWITTER_URL": "https://x.com/ROTA_finance",
